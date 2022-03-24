44153aa75f53eace14362fadbe10545836965d96
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-24 10:02:33 +0530

    adding Program.cs for prime factorial

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Prime_Factorize
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int num, i;
+            Console.WriteLine("Enter your integer");
+            num = Convert.ToInt32(Console.ReadLine());
+          
+            for(i = 2; i <= num; i++)
+            {
+                if(num%i ==0)
+                {
+                    Console.WriteLine(i + " is a factor of " + num);
+                }
+            }
+        }
+    }
+}
